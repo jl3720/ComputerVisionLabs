f4e9bbfd1c009e83f4ac2fb919db9eb264b60039
jl3720 <jameszcliu@gmail.com>
2023-11-09 19:45:39 +0100

    Update vgg model with init and forward methods.

--- exercise4_object_recognition_code/models/vgg_simplified.py
+++ exercise4_object_recognition_code/models/vgg_simplified.py
@@ -31,9 +31,44 @@ class Vgg(nn.Module):
         # #     ...)
         # for all conv layers, set: kernel=3, padding=1
 
-        ...
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512, fc_layer),
+            nn.ReLU(),
+            nn.Linear(fc_layer, classes)
+        )
 
         for m in self.modules():
+            # print("##############")
+            # print("module:")
+            # print(m)
+            # print("##############\n")
+
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
@@ -47,7 +82,19 @@ class Vgg(nn.Module):
         """
         score = None
         # todo
-        ...
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        x = self.conv_block5(x)
+        score = self.classifier(x)
 
         return score
 
+if __name__ == "__main__":
+    net = Vgg(512, 10)
+    print(net,"\n\n")
+
+    score = net(torch.Tensor(128, 3, 32, 32))
+    print(score.shape)
+    print(score[0])
