8a0129096ec3e88c085290bba562c701b2066a8f
jl3720 <jameszcliu@gmail.com>
2023-11-24 02:11:46 +0100

    Zipped submission.

---
+++ lab05-image-segmentation/submission/mean-shift.py
@@ -0,0 +1,79 @@
+import time
+import numpy as np
+
+# run `pip install scikit-image` to install skimage, if you haven't done so
+from skimage import io, color
+from skimage.transform import rescale
+
+def distance(x, X):
+    """
+    Compute the distance between a given point and all other points
+    that are within a specific radius. Consider radius of +inf.
+    """
+    # X is an array of shape (N, 3), where N is the number of points
+    # x is a point of shape (3,)
+    # return an array of shape (N,) containing the distance between x and each point in X
+    return np.linalg.norm(X - x, axis=1)
+
+def gaussian(dist, bandwidth):
+    """
+    Compute the weights of points according to the distance computed
+    by the distance function.
+    """
+    # dist is an array of shape (N,) containing the distances between a point and all other points
+    # bandwidth is a scalar representing the bandwidth parameter
+    # return an array of shape (N,) containing the weights computed using the gaussian function
+    return np.exp(-0.5 * (dist / bandwidth) ** 2)
+
+def update_point(weight, X):
+    """
+    Update the point position according to weights computed
+    from the gaussian function.
+    """
+    # weight is an array of shape (N,) containing the weights computed using the gaussian function
+    # X is an array of shape (N, 3) representing the points
+    # return the updated point position
+    return np.sum(weight[:, np.newaxis] * X, axis=0) / np.sum(weight)
+
+def meanshift_step(X, bandwidth=2.5):
+    # raise NotImplementedError('meanshift_step function not implemented!')
+    for i in range(X.shape[0]):
+        dist = distance(X[i], X)
+        weight = gaussian(dist, bandwidth)
+        X[i] = update_point(weight, X)
+    return X
+
+def meanshift(X):
+    BANDWIDTH = 2.5
+    for _ in range(20):
+        X = meanshift_step(X, BANDWIDTH)
+    return X
+
+scale = 0.5    # downscale the image to run faster
+
+# Load image and convert it to CIELAB space
+image = rescale(io.imread('eth.jpg'), scale, channel_axis=-1)
+image_lab = color.rgb2lab(image)
+shape = image_lab.shape # record image shape
+image_lab = image_lab.reshape([-1, 3])  # flatten the image
+
+# print(shape)
+# print(image_lab.shape)
+
+# Run your mean-shift algorithm
+t = time.time()
+X = meanshift(image_lab)
+t = time.time() - t
+print ('Elapsed time for mean-shift: {}'.format(t))
+
+# Load label colors and draw labels as an image
+colors = np.load('colors.npz')['colors']
+colors[colors > 1.0] = 1
+colors[colors < 0.0] = 0
+
+centroids, labels = np.unique((X / 4).round(), return_inverse=True, axis=0)
+
+result_image = colors[labels].reshape(shape)
+result_image = rescale(result_image, 1 / scale, order=0, channel_axis=-1)     # resize result image to original resolution
+result_image = (result_image * 255).astype(np.uint8)
+io.imsave('result.png', result_image)
---
+++ lab05-image-segmentation/submission/seg_net_lite.py
@@ -0,0 +1,113 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import logging
+
+import torch
+import torch.nn as nn
+from collections import OrderedDict
+
+logger = logging.getLogger(__name__)
+
+
+class SegNetLite(nn.Module):
+
+    def __init__(self, kernel_sizes=[3, 3, 3, 3], down_filter_sizes=[32, 64, 128, 256],
+            up_filter_sizes=[128, 64, 32, 32], conv_paddings=[1, 1, 1, 1],
+            pooling_kernel_sizes=[2, 2, 2, 2], pooling_strides=[2, 2, 2, 2], **kwargs):
+        """Initialize SegNet Module
+
+        Args:
+            kernel_sizes (list of ints): kernel sizes for each convolutional layer in downsample/upsample path.
+            down_filter_sizes (list of ints): number of filters (out channels) of each convolutional layer in the downsample path.
+            up_filter_sizes (list of ints): number of filters (out channels) of each convolutional layer in the upsample path.
+            conv_paddings (list of ints): paddings for each convolutional layer in downsample/upsample path.
+            pooling_kernel_sizes (list of ints): kernel sizes for each max-pooling layer and its max-unpooling layer.
+            pooling_strides (list of ints): strides for each max-pooling layer and its max-unpooling layer.
+        """
+        super(SegNetLite, self).__init__()
+        self.num_down_layers = len(kernel_sizes)
+        self.num_up_layers = len(kernel_sizes)
+
+        input_size = 3 # initial number of input channels
+        # Construct downsampling layers.
+        # As mentioned in the assignment, blocks of the downsampling path should have the
+        # following output dimension (igoring batch dimension):
+        # 3 x 64 x 64 (input) -> 32 x 32 x 32 -> 64 x 16 x 16 -> 128 x 8 x 8 -> 256 x 4 x 4
+        # each block should consist of: Conv2d->BatchNorm2d->ReLU->MaxPool2d
+        layers_conv_down = []
+        layers_bn_down = []
+        layers_pooling = []
+        # raise NotImplementedError('Downsampling layers are not implemented!')
+        for i, out_channels in enumerate(down_filter_sizes):
+            layers_conv_down.append(nn.Conv2d(input_size, out_channels, kernel_sizes[i], padding=conv_paddings[i]))
+            layers_bn_down.append(nn.BatchNorm2d(out_channels))
+            layers_pooling.append(nn.MaxPool2d(pooling_kernel_sizes[i], stride=pooling_strides[i], return_indices=True))  # store indices for Unpooling
+            input_size = out_channels
+
+        # Convert Python list to nn.ModuleList, so that PyTorch's autograd
+        # package can track gradients and update parameters of these layers
+        self.layers_conv_down = nn.ModuleList(layers_conv_down)
+        self.layers_bn_down = nn.ModuleList(layers_bn_down)
+        self.layers_pooling = nn.ModuleList(layers_pooling)
+
+        # Construct upsampling layers
+        # As mentioned in the assignment, blocks of the upsampling path should have the
+        # following output dimension (igoring batch dimension):
+        # 256 x 4 x 4 (input) -> 128 x 8 x 8 -> 64 x 16 x 16 -> 32 x 32 x 32 -> 32 x 64 x 64
+        # each block should consist of: MaxUnpool2d->Conv2d->BatchNorm2d->ReLU
+        layers_conv_up = []
+        layers_bn_up = []
+        layers_unpooling = []
+        # raise NotImplementedError('Upsampling layers are not implemented!')
+        for i, out_channels in enumerate(up_filter_sizes):
+            layers_unpooling.append(nn.MaxUnpool2d(pooling_kernel_sizes[i], stride=pooling_strides[i]))
+            layers_conv_up.append(nn.Conv2d(input_size, out_channels, kernel_sizes[i], padding=conv_paddings[i]))
+            layers_bn_up.append(nn.BatchNorm2d(out_channels))
+            input_size = out_channels
+
+        # Convert Python list to nn.ModuleList, so that PyTorch's autograd
+        # can track gradients and update parameters of these layers
+        self.layers_conv_up = nn.ModuleList(layers_conv_up)
+        self.layers_bn_up = nn.ModuleList(layers_bn_up)
+        self.layers_unpooling = nn.ModuleList(layers_unpooling)
+
+        self.relu = nn.ReLU(True)
+
+        # Implement a final 1x1 convolution to to get the logits of 11 classes (background + 10 digits)
+        # raise NotImplementedError('Final convolution layer is not implemented!')
+        self.logits = nn.Conv2d(in_channels=input_size, out_channels=11, kernel_size=1, padding=0)
+        # self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        # Downsample path
+        indices = []
+        for i in range(self.num_down_layers):
+            x = self.layers_conv_down[i](x)
+            x = self.layers_bn_down[i](x)
+            x = self.relu(x)
+            x, ind = self.layers_pooling[i](x)
+            indices.append(ind)
+
+        # Upsample path
+        for i in range(self.num_up_layers):
+            ind = indices.pop()  # start with last maxpooling indices
+            x = self.layers_unpooling[i](x, ind)
+            x = self.layers_conv_up[i](x)
+            x = self.layers_bn_up[i](x)
+            x = self.relu(x)
+
+        # Final convolution
+        x = self.logits(x)
+        # x = self.softmax(x)
+
+        return x
+
+
+def get_seg_net(**kwargs):
+
+    model = SegNetLite(**kwargs)
+
+    return model
